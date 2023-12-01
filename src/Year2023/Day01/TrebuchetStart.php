7089017c02b23bdb163e65a9a1f7cd68d5e32a09
Franziska Frenzel <f.frenzel@axilaris.de>
2023-12-01 12:05:40 +0100

    First Day in 2023

--- src/Year2023/Day01/TrebuchetStart.php
+++ src/Year2023/Day01/TrebuchetStart.php
@@ -8,8 +8,12 @@ require '../../../vendor/autoload.php';
 $trebuchetExampleOne = new Trebuchet('example_one.txt');
 $trebuchetExampleOne->output();
 
+echo "\n\n";
+
 $trebuchetExampleTwo = new Trebuchet('example_two.txt');
 $trebuchetExampleTwo->output();
+
+echo "\n\n";
 
 $trebuchet = new Trebuchet('input.txt');
 $trebuchet->output();
