3416b38af48c460f3e74a95a0b921a9e55bfe7db
ArishaKyoko <mail@arishak.de>
2023-12-04 09:19:07 +0100

    day02_2023

---
+++ tests/Year2023/Day02/CubeConundrumTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace AoCTest\Year2023\Day02;
+
+use AoC\Enums\Files;
+use AoC\Year2023\Day02\CubeConundrum;
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+class CubeConundrumTest extends TestCase
+{
+    public function test()
+    {
+        $example = new CubeConundrum(Files::EXAMPLE);
+        $this->assertEquals(8, $example->partOne());
+        $this->assertEquals(2286, $example->partTwo());
+
+        $input = new CubeConundrum(Files::INPUT);
+        $this->assertEquals(2528, $input->partOne());
+        $this->assertEquals(67363, $input->partTwo());
+    }
+
+}
