ad3467a8b76fac66372d14232c1153bcf1fa092d
ArishaKyoko <mail@arishak.de>
2024-12-02 12:40:40 +0100

    Day 01, 2024

---
+++ src/Year2024/Day01/HistorianHysteria.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace AoC\Year2024\Day01;
+
+use AoC\Base;
+use AoC\Enums\Files;
+use Illuminate\Support\Collection;
+use function explode;
+
+class HistorianHysteria extends Base
+{
+	private Collection $left;
+	private Collection $right;
+
+	public function __construct(Files $filename)
+	{
+		parent::__construct($filename);
+
+		$left = new Collection();
+		$right = new Collection();
+		foreach ($this->fileArray as $string) {
+			$explode = explode('   ', $string);
+			$left->push((int) $explode[0]);
+			$right->push((int) $explode[1]);
+		}
+
+		$this->left = $left;
+		$this->right = $right;
+	}
+
+	public function partOne(): int
+	{
+		$left = $this->left->sort()->values();
+		$right = $this->right->sort()->values();
+
+		$sum = 0;
+		for ($i = 0; $i < count($left); $i++) {
+			if ($left[$i] > $right[$i]) {
+				$sum += $left[$i] - $right[$i];
+			} else {
+				$sum += $right[$i] - $left[$i];
+			}
+		}
+
+		return $sum;
+	}
+
+	public function partTwo(): int
+	{
+		$sum = 0;
+		foreach ($this->left as $number) {
+			$countRight = $this->right->filter(function (int $i) use ($number) { return $i === $number; })->count();
+			$sum += $number * $countRight;
+		}
+
+		return $sum;
+	}
+}
